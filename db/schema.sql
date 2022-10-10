e80f6fdb89e502bd0f87cca4fac68acd6dc4c7f4
Anthony Santo <tonymsanto@gmail.com>
2022-10-10 15:16:57 -0600

    Set directory and schema.sql and installed dependencies

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+CREATE TABLE department (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(50)
+);
+CREATE TABLE roles (
+    id INT PRIMARY KEY,
+    title VARCHAR(50),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY(role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY(manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
